03b992748947f75d6691e99a5ffe07493dc90335
08madison <58222667+08madison@users.noreply.github.com>
2026-03-29 22:21:45 +0800

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from '@/components/layout/Layout';
+import { Home } from '@/pages/Home';
+import { ProductsPage } from '@/pages/Products';
+import { Publish } from '@/pages/Publish';
+import { Login } from '@/pages/Login';
+import { Register } from '@/pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="publish" element={<Publish />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
